cad850493a538f2c568ba6df6820f1fdc18d91d8
hamail <sbshamail123@gmail.com>
2025-06-04 17:56:01 +0500

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import Header from "@/components/header";
 import { FeatureShowcase } from "./_page_component/_home/feature_showcase";
 import { TrainingEcosystem } from "./_page_component/_home/TrainingEcosystem";
-import LifeCycleManagement from "./_page_component/_home/LifeCycleManagement";
+// import LifeCycleManagement from "./_page_component/_home/LifeCycleManagement";
 import { CustomerEngagement } from "./_page_component/_home/CustomerEngagement";
 import OnePlatform from "./_page_component/_home/OnePlatform";
 import { CustomerRetained } from "./_page_component/_home/CustomerRetained";
