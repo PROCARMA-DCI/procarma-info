f4a43874c96d5ebf4673aaaaffae9891d7f94356
sbshamail123 <sbshamail123@gmail.com>
2025-07-14 16:32:27 +0500

    update

---
+++ src/app/fonts/indivisible/indivisible.ts
@@ -0,0 +1,35 @@
+import localFont from "next/font/local";
+
+const indivisible = localFont({
+  variable: "--font-sans",
+  display: "swap",
+  src: [
+    {
+      path: "./fonnts.com-Indivisible_Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonnts.com-Indivisible_Medium.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonnts.com-Indivisible_Black_Italic.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonnts.com-Indivisible_Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonnts.com-Indivisible_Bold_Italic.otf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+});
+
+export default indivisible;
