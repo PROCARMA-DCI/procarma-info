8e65f6c2881ceca13a0051c5b49c454b5a577775
sbshamail123 <sbshamail123@gmail.com>
2025-07-18 15:24:03 +0500

    update

--- src/app/_page_component/home/Partnered.tsx
+++ src/app/_page_component/home/Partnered.tsx
@@ -12,7 +12,7 @@ import {
 import Image from "next/image";
 const Partnered = ({ containerVariants }: any) => {
   const plugin = React.useRef(
-    Autoplay({ delay: 1000, stopOnInteraction: true })
+    Autoplay({ delay: 2000, stopOnInteraction: false })
   );
 
   return (
@@ -35,20 +35,23 @@ const Partnered = ({ containerVariants }: any) => {
             }}
             className="w-full"
             plugins={[plugin.current]}
-            onMouseEnter={plugin.current.stop}
+            // onMouseEnter={plugin.current.stop}
             onMouseLeave={plugin.current.reset}
-            onTouchStart={plugin.current.stop} // 👈 Stops on mobile touch
+            // onTouchStart={plugin.current.stop} // 👈 Stops on mobile touch
             onTouchEnd={plugin.current.reset}
           >
             <CarouselContent className="">
               {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index} className="basis-1/5 ml-20">
+                <CarouselItem
+                  key={index}
+                  className="basis-1/5 ml-20 select-none"
+                >
                   <Image
                     src={`/partners/${index + 1}.png`}
                     alt={`partners ${index + 1}`}
                     width={2000}
                     height={2000}
-                    className="w-full rounded-lg grayscale hover:grayscale-0 transition-all duration-300 scale-75"
+                    className="w-full rounded-lg grayscale opacity-70 hover:grayscale-0 transition-all duration-300 scale-75"
                   />
                 </CarouselItem>
               ))}
