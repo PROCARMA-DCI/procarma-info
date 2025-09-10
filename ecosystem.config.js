5595fd5016c14934f7731e9c486efc1c6433f6d1
sbshamail123 <sbshamail123@gmail.com>
2025-09-10 18:03:36 +0500

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,10 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 4001,
+        SENDGRID_SMTP: process.env.SENDGRID_SMTP,
+        SENDGRID_USER: process.env.SENDGRID_USER,
+        SENDGRID_PASS: process.env.SENDGRID_PASS,
+        DEFAULT_FROM_EMAIL: process.env.DEFAULT_FROM_EMAIL,
       },
     },
   ],
