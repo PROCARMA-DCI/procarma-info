09d257535548b44a210f8f6156deb70c996800b0
sbshamail123 <sbshamail123@gmail.com>
2025-10-01 12:41:17 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import Script from "next/script";
+
 const font = Poppins({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
@@ -18,8 +20,28 @@ import { ThemeProvider } from "./theme-provider";
 // });
 
 export const metadata: Metadata = {
-  title: "Procarma Info",
-  description: "Procarma Info",
+  title: "Home - PROCARMA",
+  description:
+    "The first and only Dealer Branded App platform with telematics and AI based customer retention tool. Autoverse combines the power of GPS Telematics and the PROCARMA retention platform making it the most versatile and must have customer retention platforms for Dealerships who want to serve their customers best",
+  authors: [{ name: "" }], // you can put author name here
+  alternates: {
+    canonical: "https://procarma.info/",
+  },
+  openGraph: {
+    locale: "en_US",
+    type: "website",
+    title: "Home - PROCARMA",
+    description:
+      "The first and only Dealer Branded App platform with telematics and AI based customer retention tool. Autoverse combines the power of GPS Telematics and the PROCARMA retention platform making it the most versatile and must have customer retention platforms for Dealerships who want to serve their customers best",
+    url: "https://procarma.info/",
+    siteName: "Autoverse",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
+  verification: {
+    google: "2xywOhpL5c8lB5oKeiLEJU5tqSBOkvTJ3s-bjxxAlLw",
+  },
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" }, // fallback for all browsers
@@ -36,6 +58,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={indivisible.className}>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <ThemeProvider
           attribute="class"
@@ -47,6 +70,20 @@ export default function RootLayout({
           <Layout>{children}</Layout>
           <Toaster />
         </ThemeProvider>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VDN6KPXSG8"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VDN6KPXSG8');
+          `}
+        </Script>
       </body>
     </html>
   );
